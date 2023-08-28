9bba4547395d0bf8860c996c19927b38e6a8f613
Piotr Wilk <piotrix12@gmail.com>
2023-08-28 18:03:55 +0200

    Add project files.

---
+++ NZWalksAPI/Models/Domain/Walk.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace NZWalksAPI.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKM { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+
+    }
+}
